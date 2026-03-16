027bf643b67addcc31f7559fb09f45d7395f3164
pciudad <paulociudadq@gmail.com>
2026-03-15 22:12:23 -0500

    subida de MAS TABLAS

---
+++ SQLS/2.sql
@@ -0,0 +1,22 @@
+CREATE TABLE cotizacion_cabecera (
+    id_cotizacion             NUMBER(5) PRIMARY KEY NOT NULL,
+    fecha_cotizacion          DATE NOT NULL,
+    IDCORRENTISTA             NUMBER NOT NULL,
+    atencion_a                VARCHAR2(150),
+    referencia                VARCHAR2(500),
+    nro_oferta_tecnica        VARCHAR2(100),
+    forma_pago                VARCHAR2(200),
+    lugar_entrega             VARCHAR2(200),
+    validez_dias              NUMBER(5) DEFAULT 15,
+    garantia                  VARCHAR2(300),  --QUIZA TENGAMOS TABLA PARA ESTO
+    observaciones             VARCHAR2(2000),
+    id_estado_cotizacion      NUMBER NOT NULL,
+    subtotal                  NUMBER(15,2) DEFAULT 0 NOT NULL,
+    igv                       NUMBER(15,2) DEFAULT 0 NOT NULL,
+    total                     NUMBER(15,2) DEFAULT 0 NOT NULL,
+    fecha_creacion        DATE DEFAULT SYSDATE NOT NULL,
+    usuario_creacion      VARCHAR2(100) DEFAULT USER NOT NULL,
+    fecha_modificacion    DATE,
+    usuario_modificacion  VARCHAR2(100), 
+    ESTADO NUMBER(1) DEFAULT 0 NOT NULL,
+);
